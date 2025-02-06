cef142e9df6ab7c532907bdaa4103c194f617262
NikolasBarrera <nikolasbarrera65@gmail.com>
2025-02-06 16:54:22 -0500

    Add files via upload

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/reto00.py
@@ -0,0 +1,38 @@
+# EJERCICIO 1 
+
+# https://www.python.org /
+
+
+# EJERCICIO 2
+
+# Comentario en una sola Linea
+
+'''
+Comentarios en varias lineas con comillas simples
+'''
+
+"""
+Comentario en varias lineas con comillas dobles
+"""
+
+
+# EJERCICIO 3
+
+My_variable = "Esta es una variable"
+My_variable = "Este es el nuevo valor de mi variable"
+
+# Python no tiene Constantes
+
+
+# EJERCICIO 4
+
+my_str = "Esto es una cadena de texto"
+my_int = 23
+my_float = 24.8
+my_bool = False
+my_bool = True
+
+
+# EJERCICIO 5
+
+print("¡Hola, Python!")
