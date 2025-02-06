e9a299f277ba4ddc05c0414fdf5bc11c705d2293
NikolasBarrera <nikolasbarrera65@gmail.com>
2025-02-06 15:31:15 -0500

    NikolasBarrera.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/NikolasBarrera.py
@@ -0,0 +1,34 @@
+# https://www.python.org/
+
+#Este es un comentario en una sola linea (el más comun)
+
+# Este comentario
+# es de varias lineas
+
+"""
+Este es un comentario en varias
+lineas (con comillas dobles)
+"""
+
+'''
+Este es un comentario en varias
+lineas (con comillas simples)
+'''
+
+# Variables en Python
+mi_variable = "esta es mi primer variable"
+
+#Constante, En python no existen las constantes, pero por conveción se ponen en letras mayusculas,
+#para indicar que no se deben modificar.Aunque aun asi se puede modificar.
+PI=3.1416 
+
+# Tipos de variables primitivas
+entero= 24 #Entero (int)
+
+flotante= 8.24 #flotante (float)
+
+cadena_de_texto= "Hola, me llamo Nikolas" #cadena de texto (str)
+
+booleano= True #Booleano (bool)
+
+print ("Hola, Python")
